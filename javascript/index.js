7e33fdff33fb7bd769eac9d7d5257895e866b5ad
alja1313 <olgaslahota@gmail.com>
2024-05-08 19:30:37 +0200

    Add Multiple location JavaScript

--- javascript/index.js
+++ javascript/index.js
@@ -7,7 +7,7 @@ function updateTime() {
 
   londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
   londonTimeElement.innerHTML = londonTime.format(
-    "h:mm:ss:SSS [<small>]A[</small>]"
+    "h:mm:ss [<small>]A[</small>]"
   );
 
   //Paris
@@ -24,3 +24,24 @@ function updateTime() {
 
 updateTime();
 setInterval(updateTime, 100);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+}
