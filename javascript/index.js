46e231dcb888a3afd5665549958bd572f46bf663
alja1313 <olgaslahota@gmail.com>
2024-05-09 15:31:33 +0200

    Add Tokyo city to the main cities

--- javascript/index.js
+++ javascript/index.js
@@ -18,6 +18,15 @@ function updateTime() {
 
   parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
   parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+
+  //Tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
 }
 
 updateTime();
